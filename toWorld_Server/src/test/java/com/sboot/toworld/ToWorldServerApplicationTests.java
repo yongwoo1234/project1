0310cae3db5354a3b29f4f63665fc275c2105791
yongwoo96 <50695269+yongwoo96@users.noreply.github.com>
2022-02-14 12:07:13 +0900

    toWorld_Server

---
+++ toWorld_Server/src/test/java/com/sboot/toworld/ToWorldServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sboot.toworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToWorldServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
